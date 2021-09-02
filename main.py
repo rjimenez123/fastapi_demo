2f856b19ba946bb4bb44dde2bdf0327ab36d92c8
ANAΓKH <a20152428@pucp.pe>
2021-09-02 16:53:02 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+# import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host='127.0.0.1', port=8000, workers=8, debug=True)
