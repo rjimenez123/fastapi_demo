818f148ecf9d7222060dbd9f302264ec14ec7e61
ANAΓKH <a20152428@pucp.pe>
2021-09-09 10:54:16 -0500

    base structure

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
-# import uvicorn
+from random import randint
+from typing import Optional
+from pydantic import BaseModel
+
+class Product(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    stock: Optional[int] = 0
 
 app = FastAPI()
 
@@ -7,6 +15,22 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+# @app.get("/items/airpods")
+# async def read_airpods_id():
+#     return {"item_id": 2244}
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host='127.0.0.1', port=8000, workers=8, debug=True)
+@app.get("/items/{item_name}")
+async def read_item_id(item_name):
+    return {item_name: randint(100000, 999999)}
+
+@app.get("/products/{product_id}")
+async def read_product_id(product_id: int):
+    return {"item_id": product_id}
+
+@app.get("/items/airpods")
+async def read_airpods_id():
+    return {"item_id": 2244}
+
+@app.post("/product/")
+async def create_item(product: Product):
+    return product
